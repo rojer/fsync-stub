2d7ce184044d4a0ea6adca06e32cb9bc1abe22e7
rojer <rojer@rojer.me>
2018-09-27 16:52:28 +0300

    Fix output arch

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -e -x
 
-gcc -fPIC -nodefaultlibs -shared -o libfsync-stub.so.i386-linux-gnu fsync-stub.c
-gcc -m32 -fPIC -nodefaultlibs -shared -o libfsync-stub.so.x86_64-linux-gnu fsync-stub.c
+gcc -fPIC -nodefaultlibs -shared -o libfsync-stub.so.x86_64-linux-gnu fsync-stub.c
+gcc -m32 -fPIC -nodefaultlibs -shared -o libfsync-stub.so.i386-linux-gnu fsync-stub.c
 gcc -Wall -o fsync-test fsync-test.c
 
 LD_PRELOAD=`pwd`/libfsync-stub.so.'$LIB' ./fsync-test && echo Ok
